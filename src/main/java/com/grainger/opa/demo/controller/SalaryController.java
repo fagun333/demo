00633ac36d6cf68df695e16079e4c97094c9f477
fagun333 <rubab.iut@gmail.com>
2023-09-13 13:54:00 -0500

    updated code

--- src/main/java/com/grainger/opa/demo/controller/SalaryController.java
+++ src/main/java/com/grainger/opa/demo/controller/SalaryController.java
@@ -3,15 +3,21 @@ package com.grainger.opa.demo.controller;
 import com.grainger.opa.demo.model.Salary;
 import com.grainger.opa.demo.service.SalaryService;
 import java.util.List;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SalaryController {
+
+  private static final Logger log = LoggerFactory.getLogger(SalaryController.class);
 
   @Autowired
   private SalaryService salaryService;
@@ -22,7 +28,10 @@ public class SalaryController {
   }
 
   @GetMapping("/getSalary/{employeeId}")
-  Salary getSalaryByEmployeeId(@PathVariable String employeeId) {
+  Salary getSalaryByEmployeeId(@PathVariable String employeeId, @RequestHeader Map<String, String> headers) {
+    headers.forEach((k,v) -> {
+      log.info(String.format("Header '%s' = %s", k, v));
+    });
     return salaryService.getSalaryByEmployeeId(employeeId);
   }
 
